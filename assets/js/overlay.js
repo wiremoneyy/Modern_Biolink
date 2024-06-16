6f40f28bd322aab56977515e262cb63891a50679
wiremoneyy <wire@wiremoney.me>
2024-06-16 16:25:34 -0700

    added a click to enter that plays the music

---
+++ assets/js/overlay.js
@@ -0,0 +1,8 @@
+//CREDITS TO https://github.com/Euro-pol
+$(document).ready(function(){ //CREDITS TO https://github.com/Euro-pol
+    $("#overlay").click(function(){ //CREDITS TO https://github.com/Euro-pol
+        $("#overlay").fadeOut(1000); //CREDITS TO https://github.com/Euro-pol
+        $("#audio")[0].play(); //CREDITS TO https://github.com/Euro-pol
+    }); //CREDITS TO https://github.com/Euro-pol
+}); //CREDITS TO https://github.com/Euro-pol
+//CREDITS TO https://github.com/Euro-pol
